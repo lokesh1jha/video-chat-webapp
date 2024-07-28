5c0cecbc796d71d529f99f78b27d2879f2453304
lokeshjha <lokesh1jha@gmai.com>
2024-07-28 20:29:02 +0530

    handler chat and room are done 🫡

--- internal/handlers/chat.go
+++ internal/handlers/chat.go
@@ -1,1 +1,58 @@
 package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	"videochat/pkg/chat"
+	w "videochat/pkg/webrtc"
+)
+fun RoomChat(c *fiber.Ctx) error {
+	return c.render("chat", fiber.Map{}, "layout/main")
+}
+
+
+func RoomChatWebsocket(c *websocket.Conn) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	w.RoomsLock.Lock()
+	room := w.Rooms[uuid]
+	w.RoomsLock.Unlock()
+
+	if room == nil {
+		return
+	}
+
+	if room.Hub == nil {
+		return
+	}
+
+	chat.PeerChatConn(c.Conn, room.Hub)
+}
+
+func StreamChatWebsocket(c *websocket.Conn) error {
+	suuid := c.Params("ssuid")
+
+	if suuid == "" {
+		return
+	}
+
+	w.RommsLock.Lock()
+
+	if stream, ok := w.Streams[suuid]; ok {
+		w.RommsLock.Unlock()
+		
+		if stream.Hub == nil {
+			hub := chat.NewHub()
+			stream.Hub = hub
+			go hub.Run()
+		}
+		chat.PeerChatConn(c.Conn, stream.Hub)
+		return 
+	}
+	w.RoomsLock.Unlock()
+}
+
+
--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -2,12 +2,18 @@ package handlers
 
 import (
 	"fmt"
+	"os"
 	w "videochat/pkg/webrtc"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	guuid "github.com/google/uuid"
 )
+
+type websocketMessage struct {
+	Event string `json:"event"`
+	Data  string `josn: "data"`
+}
 
 func RoomCreate(c *fiber.Ctx) error {
 	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
@@ -20,8 +26,20 @@ func Room(c *fiber.Ctx) error {
 		c.Status(400)
 		return nil
 	}
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "production" {
+		ws = "wss"
+	}
 
 	uuid, suuid, _ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebSocketAddr": fmt.Sprintf("%s://%s/room/%s/websocket", ws, c.Hostname(), uuid),
+		"RoomLink":          fmt.Sprintf("%s://%s/room/%s", c.Protocol(), , c.Hostname(), uuid),
+		"ChatWebsocketAddr": fmt.Sprintf("%s://%s/room/%s/chat/websocket", ws, c.Hostname(), uuid),
+		"ViewerWebsocket":   fmt.Sprintf("%s://%s/room/%s/viewer/websocket", ws, c.Hostname(), uuid),
+		"StreamLink":        fmt.Sprintf("%s://%s/stream/%s", ws, c.Hostname(), suuid),
+		"Type":              "room",
+	}, "layout/main")
 }
 
 func RoomWebsocket(c *websocket.Conn) error {
@@ -37,18 +55,72 @@ func RoomWebsocket(c *websocket.Conn) error {
 }
 
 func createOrGetRoom(uuid string) (string, string, Room) {
+	w.RoomsLock.Lock()
+	
+	defer w.RoomsLock.Unlock()
 
+	h := sha256.new()
+	h.write([]byte(uuid))
+	suuid := fmt.Sprintf("%x", h.sum(nil))
+
+	if room, ok := w.Rooms[uuid]; ok {
+		if _, ok := w.Stream[suuid]; !ok {
+			w.Stream[suuid] = room
+		}
+		return uuid, suuid, room
+	}
+
+	hub := chat.NewHub()
+	peer := &w.Peer{}
+	p.TrackLocals = make(map[string]*webrtc.TrackLocalStaticRTP)
+	room := &w.Room{
+		Peers: p,
+		Hub: hub,
+	}
+	w.Rooms[uuid] = room
+	w.Stream[uuid] = room
+
+	go hub.Run()
+	return uuid, suuid, room
+	
 }
 
 func RoomViewerWebsocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
 
+	w.RoomsLock.Lock()
+	if peer, ok := w.Rooms[uuid]; ok {
+		w.RoomsLock.Unlock()
+		roomViewerConn(c, peer)
+		return
+	}
+
+	w.RoomsLock.Unlock()
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peer) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
 
-}
+	ticker := time.NewTicker(1 * time.Second)
+	defer func () {
+		ticker.Stop()
+		c.Close()
+	}()
 
-type websocketMessage struct {
-	Event string `json:"event"`
-	Data  string `josn: "data"`
+	for {
+		select {
+			case <-ticker.C:
+				w, err := c.Conn.NextWriter(websocket.TextMessage)
+				if err != nil {
+					return
+				}
+				w.Write([]byte(fmt.Sprintf("%d", len(p.Connection))))
+		}
+	}
 }
